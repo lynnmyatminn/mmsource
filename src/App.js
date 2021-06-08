f5644434962fdb23af7282a40af6a03de8e51c5f
Lin Myat Min <lynnmyatminn@gmail.com>
2021-06-08 16:02:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import Courses from './components/pages/Courses';
+import Movies from './components/pages/Movies';
+import Tvseries from './components/pages/Tvseries';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/courses' component={Courses} />
+          <Route path='/movies' component={Movies} />
+          <Route path='/tvseries' component={Tvseries} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/pages/Movies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+
+function Movies() {
+  return (
+    <>
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Movies;
