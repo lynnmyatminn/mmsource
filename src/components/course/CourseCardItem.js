ef364fd2228c517fef7fc250034fa359b4269eea
Lin Myat Min <lynnmyatminn@gmail.com>
2021-06-22 15:02:13 +0700

    courses

--- src/components/course/CourseCardItem.js
+++ src/components/course/CourseCardItem.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CourseCardItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+function CourseCardItem(props) {
+
+  const { detail } = props;
+
+  const title = detail.name;
+  const overview = detail.description;
+
+  return (
+    <>
+      <div className="cards__item">
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={Object.keys(detail.categories).map((category) => detail.categories[category].name)}>
+            <img
+              className='cards__item__img'
+              alt={title}
+              src={'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5da5c84c6763cb000608c51e%2F0x0.jpg'}
+            />
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{title}</h5>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default CourseCardItem
+export default CourseCardItem;
--- src/components/pages/Courses.js
+++ src/components/pages/Courses.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../../App.css';
-import styled from 'styled-components'
+// import styled from 'styled-components';
 // import Cards from '../Cards';
 import CourseCard from '../course/CourseCard';
 import Footer from '../Footer';
@@ -43,7 +43,8 @@ class Courses extends Component {
     let views = <div />;
 
     if (!loading && data.length >= 1) {
-      views = Object.keys(data).map(course => <CourseCard key={course} details={data[course]} /> );
+      // views = Object.keys(data).map(course => <CourseCard key={course} details={data[course]} /> );
+      views = <CourseCard courses={data} />
     } else if (loading && !data.length) {
       views = (
         <div className="col-lg-12 text-center">
@@ -63,58 +64,30 @@ class Courses extends Component {
     }
 
     return (
-      <div>
-        <Container>
-      <div>
-        <div className="container-fluid text-center">
-          <div className="row text-center">
-            <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12" />
-            <div className="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-              {/* <form onSubmit={this.onSubmit}>
-                <input ref={(input) => { this.q = input; }} type="text" className="form-control searchInput" placeholder="Search Courses" />
-              </form> */}
-            </div>
-            <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12" />
-          </div>
-          <br />
-          <div className="row text-center">
-            <div className="col-lg-12">
-              <h5 className="movieTitle">
-                {/* {navTitle} */}
-              </h5>
-              <br />
-            </div>
-          </div>
-          <div className="row">
-            hi
-            {views}
-          </div>
-        </div>
-        {/* <Footer /> */}
-      </div>
-      </Container>
+      <>
+        {views}
         <Footer />
-        </div>
+        </>
     );
   }
 }
 
 export default Courses;
 
-const Container = styled.main`
-    min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px);
-    position: relative;
-    overflow-x: hidden;
-    &:before {
-        background: url("/images/home-background.png") center center / cover
-        no-repeat fixed;        
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: -1;
-    }
-`
+// const Container = styled.main`
+//     min-height: calc(100vh - 70px);
+//     padding: 0 calc(3.5vw + 5px);
+//     position: relative;
+//     overflow-x: hidden;
+//     &:before {
+//         background: url("/images/home-background.png") center center / cover
+//         no-repeat fixed;        
+//         content: "";
+//         position: absolute;
+//         top: 0;
+//         left: 0;
+//         right: 0;
+//         bottom: 0;
+//         z-index: -1;
+//     }
+// `
