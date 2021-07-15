d15d592a6081dabba0d54b15723038b0cbb4b046
Lin Myat Min <lynnmyatminn@gmail.com>
2021-07-15 22:26:41 +0700

    course detail
    Co-authored-by: Wai Yan <wanyay@users.noreply.github.com>

--- src/components/pages/CourseDetail.js
+++ src/components/pages/CourseDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import '../../App.css';
+import Footer from '../Footer';
 import CourseDataService from '../../services/course.service';
 
 class CourseDetail extends Component {
@@ -38,43 +39,91 @@ class CourseDetail extends Component {
 
         const { data } = this.state;
         const { loading } = this.state;
+        console.log(data);
+        console.log(loading);
 
         let view = <div />;
 
-        return (
-            <div className="coursedetail">
-                <h3>{data.name}</h3><br />
-                <h4>Description : {data.description}</h4><br />
-                <h4>Created by : {data.createdBy}</h4><br />
+        if (!loading && data != null) {
+            view = (
+                <div className="coursedetail">
+                    <div className="center-screen">
+                        <h3>{data.name}</h3><br />
+                        <p className="text-center">{data.description}</p><br />
+                        <h4>Created by : {data.createdBy}</h4><br />
+                    </div>
 
-                
-                {/* <h1>course detail</h1> */}
-                <div className="coursedetail__container">
-                    <div className="coursedetail__items">
-                        <div className="coursedetail__item">
-                            <div className="player__wrapper">
-                                <ReactPlayer className='react__player' controls={true} url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width='100%' height='480px' />
+                    <div className="coursedetail__container">
+                        <div className="coursedetail__items">
+                            <div className="coursedetail__item">
+                                <div className="player__wrapper">
+                                    {/* <ReactPlayer className='react__player' controls={true} url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width='100%' height='480px' /> */}
+                                    <iframe src="https://streamtape.com/e/3D6Ae0mkJyIdw7r/1._Introduction.mp4" width="100%" height="480" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>
+                                </div>
+                            </div>
+
+                            {/* session twe d mhar htae mal */}
+                            <div className="coursedetail__item">
+                                <div className="session__wrapper">
+                                    <div>
+                                        Course Content
+                                    </div>
+                                    <ul className="session__list">
+                                        {
+                                            data.sections.map((section, i) =>
+                                                <li key={i}>
+                                                    {section.name}
+                                                    <ul>
+
+                                                        {section.video.map((vid, i) =>
+                                                            <li key={i}>
+                                                                <div>
+                                                                    {vid.name}
+                                                                </div>
+
+                                                                <div>
+                                                                    {vid.embedcode}
+                                                                </div>
+                                                            </li>
+                                                        )}
+                                                    </ul>
+                                                </li>
+                                            )
+                                        }
+                                    </ul>
+
+                                </div>
                             </div>
                         </div>
-                        <div className="coursedetail__item">
-                            <div className="player__wrapper">
-                                <ReactPlayer className='react__player' controls={true} url='https://www.youtube.com/watch?v=7EhRdPulR48' width='100%' height='100%' />
-                            </div>
-                        </div>
-                        {/* <div className="coursedetail__item">
-                            <div className="player__wrapper">
-                                <ReactPlayer className='react__player' controls={true} url='https://www.youtube.com/watch?v=LQC3dBWS_FE' width='100%' height='100%' />
-                            </div>
-                        </div>
-                        <div className="coursedetail__item">
-                            <div className="player__wrapper">
-                                <ReactPlayer className='react__player' controls={true} url='https://www.youtube.com/watch?v=LQC3dBWS_FE' width='100%' height='100%' />
-                            </div>
-                        </div> */}
                     </div>
                 </div>
-            </div>
-        )
+            );
+        } else if (loading && data == null) {
+            view = (
+                <div className="col-lg-12 text-center">
+                    <i className="fa fa-2x fa-circle-o-notch fa-spin" />
+                </div>
+            );
+        } else if (!loading && data == null) {
+            view = (
+                <div className="col-lg-12">
+                    <div className="text-center">
+                        <button className="btn jumboButton">
+                            Could not get course detail!
+                        </button>
+                    </div>
+                </div>
+            );
+        }
+
+
+        return (
+            <>
+                {view}
+                <Footer />
+            </>
+        );
+
     }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDi7FjDiW3Q1gElfKebhMeCRpugIjaCY0o",
+    authDomain: "myanflix-35e8e.firebaseapp.com",
+    databaseURL: "https://myanflix-35e8e.firebaseio.com",
+    projectId: "myanflix-35e8e",
+    storageBucket: "myanflix-35e8e.appspot.com",
+    messagingSenderId: "990951883734",
+    appId: "1:990951883734:web:464751fa3fdfcef8579a77",
+    measurementId: "G-TY1985X3SR"
+  };
